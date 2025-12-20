64508b861997b4c52aef828cce305191829fd78b
abdurahman <abdurrahmanzerihun@gmail.com>
2025-12-20 04:04:56 +0300

    Create system state and storage

---
+++ src/state/state.js
@@ -0,0 +1,36 @@
+const STORAGE_KEY = 'taskManager';
+let subscribers = [];
+
+export function subscribe(fn) {
+  subscribers.push(fn);
+}
+
+function notify() {
+  subscribers.forEach(fn => fn());
+}
+
+
+let state = load();
+
+function load() {
+  return JSON.parse(localStorage.getItem(STORAGE_KEY)) || {
+    projects: [],
+    activeProjectId: null,
+    activeItemId: null
+  };
+}
+
+function save() {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(state));
+}
+
+export function getState() {
+  return structuredClone(state);
+}
+
+export function setState(mutator) {
+  mutator(state);
+  save();
+   notify();
+}
+

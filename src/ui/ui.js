498afc876c858c0632b977000e23b7de9faebd9f
abdurahman <abdurrahmanzerihun@gmail.com>
2025-12-24 09:53:05 +0300

    Remove renderTodos calls as renderItems already have it

--- src/ui/ui.js
+++ src/ui/ui.js
@@ -1,13 +1,11 @@
 // ui/ui.js
 import { renderProjects,renderActiveProject} from './display/projectDisplay';
 import { renderItems } from './display/itemDisplay';
-import { renderTodos } from './display/todoDisplay';
 import { getState } from '../state/state';
 
 export function renderUI() {
   const state = getState();
   renderProjects(state);
   renderActiveProject(state);
-  //renderItems(state);
-  //renderTodos(state);
+  renderItems(state);
 }

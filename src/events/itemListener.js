440c98ea165140c56ebd05fb58fd9e6630a55f42
abdurahman <abdurrahmanzerihun@gmail.com>
2025-12-25 08:52:21 +0300

    Create project sections using form  listener

--- src/events/itemListener.js
+++ src/events/itemListener.js
@@ -1,12 +1,14 @@
 import { createItem } from '../logic/items';
 
 export function setupItemListeners(){
+    const itemDialog=document.getElementById('item-dialog');
+    const addItemButton=document.getElementById('add-item-btn');
+    addItemButton.addEventListener('click',()=>itemDialog.showModal());
 const itemForm=document.getElementById('item-form');
 itemForm.addEventListener('submit', (e) => {
  e.preventDefault();
     const itemInput=document.getElementById('item-input')
-    const value = itemInput.value; // string;
-    createItem(value);
+    createItem(itemInput.value);
     itemInput.value = '';
 });
 }

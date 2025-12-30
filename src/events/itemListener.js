e0330e581040b011b4ca477f27edc603b67e7acb
abdurahman <abdurrahmanzerihun@gmail.com>
2025-12-30 12:32:10 +0300

    Add an event to close the add-item modal

--- src/events/itemListener.js
+++ src/events/itemListener.js
@@ -2,6 +2,9 @@ import { createItem } from '../logic/items';
 
 export function setupItemListeners(){
     const itemDialog=document.getElementById('item-dialog');
+    // close dialog
+    const closeAdditemDialog=document.getElementById('close-add-item-btn');
+    closeAdditemDialog.addEventListener('click',()=>itemDialog.close());
     const addItemButton=document.getElementById('add-item-btn');
     addItemButton.addEventListener('click',()=>itemDialog.showModal());
 const itemForm=document.getElementById('item-form');

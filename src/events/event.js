007df69448ec5790ec092b2d9b24877bd16ca67c
abdurahman <abdurrahmanzerihun@gmail.com>
2026-01-03 17:57:19 +0300

    Remove unnecessary import and function call

--- src/events/event.js
+++ src/events/event.js
@@ -1,9 +1,7 @@
 import { setupProjectListeners } from './projectListener';
 import { setupItemListeners } from './itemListener';
-import { setupTodoListeners } from './todoListener';
 
 export function setupEvents() {
   setupProjectListeners();
   setupItemListeners();
- // setupTodoListeners();
 }

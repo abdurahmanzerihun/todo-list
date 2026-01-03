963d8580a9f5191cb8130a20f4833a28749af586
abdurahman <abdurrahmanzerihun@gmail.com>
2026-01-03 17:55:54 +0300

    Style indentation of the code

--- src/events/projectListener.js
+++ src/events/projectListener.js
@@ -1,33 +1,38 @@
 import { createProject } from '../logic/projects';
 
-export function setupProjectListeners(){
-     const addProjectBtn = document.getElementById('add-project-btn');
+export function setupProjectListeners() {
+  const addProjectBtns = document.querySelectorAll('.add-project-btn');
   const dialog = document.getElementById('proj-dialog');
-//close modal
 
-const closeProjDialog=document.getElementById('close-add-proj-btn');
-closeProjDialog.addEventListener('click',()=>dialog.close());
-  if (!addProjectBtn || !dialog) {
+  // Close modal
+  const closeProjDialog = document.getElementById('close-add-proj-btn');
+  closeProjDialog.addEventListener('click', () => dialog.close());
+
+  if (!addProjectBtns || !dialog) {
     console.error('Project button or dialog not found');
     return;
   }
-
-  addProjectBtn.addEventListener('click', () => {
-    dialog.showModal();
+//Add project buttons click lister
+  addProjectBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+      dialog.showModal();
+    });
   });
-const projForm=document.getElementById('proj-form');
-projForm.addEventListener('submit', (e) => {
- e.preventDefault();
-    const projTitle=document.getElementById('proj-title')
-    const projPriority=document.getElementById('proj-priority')
-    const projDescription=document.getElementById('proj-description')
-    createProject(projTitle.value,projPriority.value,projDescription.value);
 
-    projTitle.value='';
-    projPriority.value='';
-    projDescription.value='';
-     dialog.close();
-    
-    
-});
+  const projForm = document.getElementById('proj-form');
+  projForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const projTitle = document.getElementById('proj-title');
+    const projPriority = document.getElementById('proj-priority');
+    const projDescription = document.getElementById('proj-description');
+
+    createProject(projTitle.value, projPriority.value, projDescription.value);
+
+    projTitle.value = '';
+    projPriority.value = '';
+    projDescription.value = '';
+
+    dialog.close();
+  });
 }
